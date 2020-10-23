76c9d916f50a373d8222dbd2317ba00ace387796
root <pleik@ukr.net>
2020-10-23 10:32:59 +0200

    password

--- installer.sh
+++ installer.sh
@@ -4,7 +4,9 @@ ssh ${SSH_USER}@${HOST} 'mkdir -p ~/install_wlpfo'
 
 echo "copy from nexus"
 
-ssh ${SSH_USER}@${HOST} curl -X GET -u 'wlp-auth-wlpfo-nexus-download:HcnT57MivkKxDb9e' ${Nexus} -O
+#ssh ${SSH_USER}@${HOST} curl -X GET -u 'wlp-auth-wlpfo-nexus-download:HcnT57MivkKxDb9e' ${Nexus} -O
+
+ssh ${SSH_USER}@${HOST} curl -X GET -u 'admin:admin123' ${Nexus} -O
 
 ssh ${SSH_USER}@${HOST} mv ${Nexus} /home/${SSH_USER}/install_wlpfo/
 
