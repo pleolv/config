f01031440c1e32228f8d3e850e048d4944d2fdf1
root <pleik@ukr.net>
2020-10-23 10:16:54 +0200

    installer.sh

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@ ssh ${SSH_USER}@${HOST} 'mkdir -p ~/install_wlpfo'
 
 echo "copy from nexus"
 
-ssh ${SSH_USER}@${HOST} curl -X GET -u 'wlp-auth-wlpfo-nexus-download:HcnT57MivkKxDb9e' https://nexus.kazan.priv.atos.fr/repository/wlp-auth-wlpfo-nexus-releases/net/atos/wlp/auth/wlp-fo/post_install/5.12.0.24/post_install-5.12.0.24.zip -O
+ssh ${SSH_USER}@${HOST} curl -X GET -u 'wlp-auth-wlpfo-nexus-download:HcnT57MivkKxDb9e' ${Nexus} -O
 
-ssh ${SSH_USER}@${HOST} mv post_install-5.12.0.24.zip /home/${SSH_USER}/install_wlpfo/
+ssh ${SSH_USER}@${HOST} mv ${Nexus} /home/${SSH_USER}/install_wlpfo/
 
